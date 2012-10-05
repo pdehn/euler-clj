7efbeeea21750a856dd40050920208c6a2a70ba4
Patrick DeHaan <patrick.dehaan@soartech.com>
2012-10-05 12:40:25 -0400

    21

--- src/euler/problem21.clj
+++ src/euler/problem21.clj
@@ -1,2 +1,24 @@
-(ns euler.problem21)
+(ns euler.problem21
+  (:use clojure.set))
 
+(defn divisors
+  [n]
+  (filter #(= 0 (mod n %)) (range 1 (inc (quot n 2)))))
+
+(defn d
+  [n]
+  (reduce + (divisors n)))
+
+(defn amicable-pairs
+  [a]
+  (let [b  (d a)
+        a' (d b)]
+    (if (and (= a' a)
+             (not= a b))
+      #{a b}
+      #{})))
+
+(defn solve
+  []
+  (reduce + (into []
+                  (apply union (map amicable-pairs (range 1 10001))))))
