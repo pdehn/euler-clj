8186c9ec9e4f6c33e840ca9645b0b1765ae5e7ba
Patrick DeHaan <patrick.dehaan@soartech.com>
2012-10-05 13:40:11 -0400

    sum/fact(orial) util functions

--- src/euler/util.clj
+++ src/euler/util.clj
@@ -1,5 +1,13 @@
 (ns euler.util)
 
+(defn sum
+  [nums]
+  (reduce + nums))
+
+(defn fact
+  [n]
+  (reduce *' (range 1 (inc n))))
+
 (defn num->digits
   [num]
   (map #(- (int %) (int \0)) (into [] num)))
